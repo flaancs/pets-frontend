f8fdf1246366f6f12cd58d533647a7025addb5b1
Gustavo Antonio <gustavo1998antonio@gmail.com>
2023-12-26 13:43:19 -0300

    feat: hook to manage pets table logic

---
+++ hooks/usePetsTable.ts
@@ -0,0 +1,79 @@
+import { Pet } from "@/types/pet";
+import { useEffect, useState } from "react";
+import { debounce } from "lodash";
+import { useUser } from "@/context/user.context";
+import { TYPE_TABS } from "@/utils/constants";
+import axiosClient from "@/utils/axios.client";
+
+export interface usePetsTableProps {
+  pets: Pet[];
+  pages: number;
+  count: number;
+}
+
+export const usePetsTable = ({ pets, pages, count }: usePetsTableProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<Pet[]>(pets);
+  const [page, setPage] = useState<number>(1);
+  const [perPage, setPerPage] = useState<number>(10);
+  const [search, setSearch] = useState<string>("");
+  const [type, setType] = useState<string>("all");
+  const [totalPages, setTotalPages] = useState<number>(pages);
+  const [totalCount, setTotalCount] = useState<number>(count);
+  const { user } = useUser();
+
+  useEffect(() => {
+    axiosClient
+      .get("/pets", {
+        params: {
+          page,
+          pageSize: perPage,
+          name: search,
+          ...(type !== "all" && { type }),
+        },
+      })
+      .then((response) => {
+        const { pets, pages, count } = response.data;
+        setData(pets);
+        setTotalPages(pages);
+        setTotalCount(count);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [page, perPage, search, type]);
+
+  const handleDebounceSearch = debounce((value) => {
+    setSearch(value);
+    setPage(1);
+  }, 500);
+
+  const handleChangePerPage = (value: number) => {
+    setPerPage(value);
+    setPage(1);
+  };
+
+  const handleChangeType = (value: number) => {
+    setType(TYPE_TABS[value]);
+    setPage(1);
+  };
+
+  return {
+    loading,
+    data,
+    page,
+    perPage,
+    search,
+    type,
+    totalPages,
+    totalCount,
+    user,
+    handleDebounceSearch,
+    handleChangePerPage,
+    handleChangeType,
+    setPage,
+  };
+};
