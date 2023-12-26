f4c3a0f777aa4c25526a8958912d6edb995508d7
Gustavo Antonio <gustavo1998antonio@gmail.com>
2023-12-26 14:54:54 -0300

    chore: add new method to create pets

--- hooks/usePetsTable.ts
+++ hooks/usePetsTable.ts
@@ -1,5 +1,5 @@
 import { Pet } from "@/types/pet";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { debounce } from "lodash";
 import { useUser } from "@/context/user.context";
 import { TYPE_TABS } from "@/utils/constants";
@@ -22,7 +22,7 @@ export const usePetsTable = ({ pets, pages, count }: usePetsTableProps) => {
   const [totalCount, setTotalCount] = useState<number>(count);
   const { user } = useUser();
 
-  useEffect(() => {
+  const fetchPets = useCallback(() => {
     axiosClient
       .get("/pets", {
         params: {
@@ -46,6 +46,8 @@ export const usePetsTable = ({ pets, pages, count }: usePetsTableProps) => {
       });
   }, [page, perPage, search, type]);
 
+  useEffect(() => fetchPets(), [fetchPets, page, perPage, search, type]);
+
   const handleDebounceSearch = debounce((value) => {
     setSearch(value);
     setPage(1);
@@ -59,6 +61,16 @@ export const usePetsTable = ({ pets, pages, count }: usePetsTableProps) => {
   const handleChangeType = (value: number) => {
     setType(TYPE_TABS[value]);
     setPage(1);
+  };
+
+  const handleCreatePet = async (data: {
+    name: string;
+    type: string;
+    breed: string;
+    age: number;
+    isSterilized: boolean;
+  }) => {
+    return axiosClient.post("/create-pet", data).then(() => fetchPets());
   };
 
   return {
@@ -75,5 +87,6 @@ export const usePetsTable = ({ pets, pages, count }: usePetsTableProps) => {
     handleChangePerPage,
     handleChangeType,
     setPage,
+    handleCreatePet,
   };
 };
